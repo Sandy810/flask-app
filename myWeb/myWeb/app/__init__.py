65998ce0b30f8df4a5ee8cfac6558bd208b549a9
Sandy810 <sandy59433@gmail.com>
2024-12-17 17:02:15 +0800

    Delete myWeb directory

--- myWeb/myWeb/app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from app.route import hello_world, index
-
-def create_app():
-    app = Flask(__name__)
-    app.add_url_rule('/', '/', hello_world)
-    app.add_url_rule('/index', 'index', index)
-    return app
--- myWeb/myWeb/app/route.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template
-
-def hello_world():
-    return "Hello, MVC框架!"
-
-def index():
-    title ="智能垃圾分類系統"
-    big_word ="歡迎使用智能垃圾分類系統！"
-    return render_template('index.html',title=title,big_word=big_word) 
